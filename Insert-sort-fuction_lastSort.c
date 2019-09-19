a653835f3f76bff7de05ef844609c6fc3b0fd573
Hafid Oxte <47432540+HafidOD@users.noreply.github.com>
2019-09-18 20:31:19 -0500

    Update Insert-sort-fuction_lastSort.c

--- Insert-sort-fuction_lastSort.c
+++ Insert-sort-fuction_lastSort.c
@@ -1,37 +1,32 @@
 #include <math.h> 
 #include <stdio.h> 
-
 // Funcion para imprimir el array
 void printArray(int arr[], int n) { 
 	int i; 
 	for (i = 0; i < n; i++) 
 		printf("%d ", arr[i]); 
-	printf("\n"); 
+	printf("\n");
 }
 
 // Funcion para ordenar el ultimo elemento m
 void lastSort(int arr[], int m) {
-	int i, key, j; 
-	for (i = 1; i < m; i++) { 
-		key = arr[i]; 
-		j = i - 1; 
-		
-		/* Move elements of arr[0..i-1], that are 
-		greater than key, to one position ahead 
-		of their current position */
-		while (j >= 0 && arr[j] > key) { 
-			arr[j + 1] = arr[j]; 
-			j = j - 1; 
-		} 
-		arr[j + 1] = key;
+	
+	int key, j; 
+	key = arr[m];
+	j = m - 1; 
+	//printArray(arr, m);
+	while (j >= 0 && arr[j] > key) { 
+		arr[j + 1] = arr[j]; 
+		j = j - 1; 
 	} 
+	arr[j + 1] = key;	
 	printArray(arr, m);
 }
 
 // Funcion que define el limite m para la funcion lastSort()
 void insertionSort(int arr[], int n) {
 	int i; 
-	for (i = 2; i <= n; i++) {
+	for (i = 1; i < n; i++) {
 		lastSort(arr, i);
 	}
 } 
@@ -39,8 +34,8 @@ void insertionSort(int arr[], int n) {
 /* Driver program to test insertion sort */
 int main() { 
 	
-	int arr[] = { 12, 11, 13, 5, 6 }; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
+	int arr[] = {4, 3, 2, 10, 12, 22, 5, 6}; 
+	int n = sizeof(arr)/sizeof(arr[0]);
 	
 	printf("Array original:\n");
 	printArray(arr, n);
@@ -49,7 +44,7 @@ int main() {
 	
 	insertionSort(arr, n);
 	
-	printf("\nArray Ordenado:\n ");
+	printf("\nArray Ordenado:\n");
 	printArray(arr, n); 
 	
 	return 0; 
